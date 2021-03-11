aecc359081ae3f3a6a7e386d120c52455897ecf8
Shashidhar Reddy P <79072706+psreddy114@users.noreply.github.com>
2021-03-11 21:23:53 +0530

    Add files via upload

---
+++ Reading All File Formates in R Programming.R
@@ -0,0 +1,54 @@
+# Readubg text data fuke ubti R ebvuribne
+
+data <- read.table("C:/Users/SHASHI/OneDrive/Documents/R TEXT FILE.txt")
+print(data)
+fix(data)
+
+
+data <- read.table("C:/Users/SHASHI/oneDrive/Documents/OrdersDataSet.csv")
+print(data)
+fix(data)
+view(data)
+
+
+data <- read.csv("C:/Users/SHASHI/OneDrive/Documents/CSVSample.csv")
+fix(data)
+view(data)
+
+# Reading semi structure text file into R Environment
+
+Semistr <- read.table("C:/Users/SHASHI/OneDrive/Documents/Semistructuresample.txt",sep=",")
+fix(Semistr)
+view(Semistr)
+
+
+# Reading Semi structure text file with slash into R environment
+
+slashdata <- read.table("C:/Users/SHASHI/OneDrive/Documents/Text Data Slash Delimited File.txt",sep="/")
+View(slashdata)
+fix(slashdata)
+
+# Reading excel data file into R environment
+
+install.packages("readxl")
+library(readxl)
+
+Exceldata <- read_excel("C:/Users/SHASHI/OneDrive/Documents/ExcelSample.xlsx")
+View(Exceldata)
+
+install.packages("rjson")
+library("rjson")
+jsondata <- fromJSON(file = "C:/Users/SHASHI/OneDrive/Documents/JSONSAMPLE.json")
+
+print(jsondata)
+fix(jsondata)
+View(jsondata)
+
+json_data_frame <- as.data.frame(jsondata)
+
+print(json_data_frame)
+
+excel_data_frame <- as.data.frame(Exceldata)
+
+View(excel_data_frame)
+

188df0cb0ded15436605afb0d0501a81a205d8fa
Shashidhar Reddy P <79072706+psreddy114@users.noreply.github.com>
2021-03-11 22:07:53 +0530

    Add files via upload

--- Reading All File Formates in R Programming.R
+++ Reading All File Formates in R Programming.R
@@ -8,18 +8,18 @@ fix(data)
 data <- read.table("C:/Users/SHASHI/oneDrive/Documents/OrdersDataSet.csv")
 print(data)
 fix(data)
-view(data)
+View(data)
 
 
 data <- read.csv("C:/Users/SHASHI/OneDrive/Documents/CSVSample.csv")
 fix(data)
-view(data)
+View(data)
 
 # Reading semi structure text file into R Environment
 
 Semistr <- read.table("C:/Users/SHASHI/OneDrive/Documents/Semistructuresample.txt",sep=",")
 fix(Semistr)
-view(Semistr)
+View(Semistr)
 
 
 # Reading Semi structure text file with slash into R environment
@@ -52,3 +52,7 @@ excel_data_frame <- as.data.frame(Exceldata)
 
 View(excel_data_frame)
 
+install.packages("RCurl")
+install.packages("XML")
+install.packages("stringr")
+install.packages("plyr")
